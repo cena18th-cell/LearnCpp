ee9aefa26d4d3986c36f58c5c379480434b53244
MisakaRain <cena18th@gmail.com>
2026-06-12 14:35:31 +0800

    一点io操作

---
+++ LearnCpp_First/ConsoleApplication1/ConsoleApplication1/.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void InputNameAndDisplay_2()
+{
+    string name;
+    cout << "请输入名称: ";
+
+    cin >> name;
+
+    cout << "\n\n\n你叫：" << name << "（这是普通函数输出的）" << endl;
+}
---
+++ LearnCpp_First/ConsoleApplication1/ConsoleApplication1/1_2.cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>
+using namespace std;
+void InputNameAndDisplay_2()
+{
+    
+    cout << "这是在其他文件里面的 ";
+
+
+}

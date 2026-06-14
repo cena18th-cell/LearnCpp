5bdcd122b0290819dc3fa4e80737b014d3d04ee5
MisakaRain <cena18th@gmail.com>
2026-06-14 15:42:50 +0800

    组合按键监测

---
+++ LearnWinApi/_03/_03/_03.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main() {
+    
+    bool LastAS = false;
+    bool LastQW = false;
+    bool LastQWE = false;
+    while (1)
+    {
+        bool currenta = (GetAsyncKeyState('A') && 0x8000) != 0;
+        bool currents = (GetAsyncKeyState('S') && 0x8000) != 0;
+        bool currentComboAS = currenta && currents;
+
+        bool currentq = (GetAsyncKeyState('Q') && 0x8000) != 0;
+        bool currentw = (GetAsyncKeyState('W') && 0x8000) != 0;
+        bool currentComboQW = currentq && currentw;
+
+        
+        bool currente = (GetAsyncKeyState('E') && 0x8000) != 0;
+        bool currentComboQWE = currentq && currentw && currente;
+
+        if (currentComboAS&& !LastAS)
+        {
+            cout << "A + S" << endl;
+        }
+
+        LastAS = currentComboAS;
+
+        if (currentComboQW && !LastQW)
+        {
+            cout << "Q + W" << endl;
+        }
+
+        LastQW = currentComboQW;
+
+        
+
+        if (currentComboQWE && !LastQWE)
+        {
+            cout << "Triple Combo" << endl;
+        }
+
+        LastQWE = currentComboQWE;
+    }
+
+    return 0;
+}
+

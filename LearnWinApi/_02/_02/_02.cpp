898c88cd3e405798c5e0d9e31e3d4e4ac66b0edd
MisakaRain <cena18th@gmail.com>
2026-06-14 14:12:30 +0800

    Win Api 单个按键的检测

---
+++ LearnWinApi/_02/_02/_02.cpp
@@ -0,0 +1,57 @@
+﻿// _02.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+    bool LastA = false;
+    bool LastC = false;
+    bool LastEsc = false;
+    while (1)
+    {
+        bool CurrentA = GetAsyncKeyState('A');
+        if (CurrentA && !LastA)
+        {
+            cout << "输出B" << endl;
+            
+            Sleep(500);
+            cout << "end" << endl;
+        }
+        LastA = CurrentA;
+
+        bool CurrentC = GetAsyncKeyState('C');
+        if (CurrentC && !LastC)
+        {
+            cout << "输出d" << endl;
+
+            Sleep(500);
+            cout << "end" << endl;
+        }
+        LastC = CurrentC;
+
+        bool CurrentEsc = GetAsyncKeyState(VK_ESCAPE);
+        if (CurrentEsc && !LastEsc)
+        {
+            
+            break;
+            
+        }
+        LastEsc = CurrentEsc;
+
+    }
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件

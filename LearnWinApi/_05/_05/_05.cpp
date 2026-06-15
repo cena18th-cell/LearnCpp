fe4916d081ff0e492bcc0329b76b04ef1997fa7c
MisakaRain <cena18th@gmail.com>
2026-06-15 14:14:53 +0800

    数组 监听 输出

---
+++ LearnWinApi/_05/_05/_05.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    Sleep(3000);
+
+    INPUT inputs[4] = {};
+
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = 'A';
+
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = 'A';
+    inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    inputs[2].type = INPUT_KEYBOARD;
+    inputs[2].ki.wVk = 'B';
+
+    inputs[3].type = INPUT_KEYBOARD;
+    inputs[3].ki.wVk = 'B';
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    SendInput(4, inputs, sizeof(INPUT));
+
+    return 0;
+}
